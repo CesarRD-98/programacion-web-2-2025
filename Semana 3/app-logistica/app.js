319be1f5a5f5db3dbb65fcbb1720e9bb0a67b5c5
CesarRD-98 <dedioscesar12@gmail.com>
2025-05-06 16:32:47 -0600

    fix

--- Semana 3/app-logistica/app.js
+++ Semana 3/app-logistica/app.js
@@ -1,3 +1,42 @@
-console.log("Hello, World!");
-console.log("This is a test created from the command line in App.js file.");
+const yargs = require('yargs');
+const dayjs = require('dayjs');
+
+const argv = yargs
+    .options('peso', {
+        alias: 'p',
+        type: 'number',
+        demandOption: true,
+        describe: 'Peso del paquete en libras'
+    })
+    .option('distancia', {
+        alias: 'd',
+        describe: 'Distancia del envio en kilometros',
+        type: 'number',
+        demandOption: true
+    }).argv;
+
+
+const costoBase = 50;
+const tiempoProcesamiento = 2; // dias
+
+
+const peso = argv.peso;
+const distancia = argv.distancia;
+
+
+let pesoExcedente = peso > 5 ? (peso - 5) * 20 : 0;
+let distanciaExcedente = distancia > 50 ? (distancia - 50) * 10 : 0;
+
+
+console.log('\n++++++++++++++++++++++++++++++++++\n');
+console.log(`-- Detalles del envio --`);
+console.log(`Peso del paquete: ${peso} libras`);
+console.log(`Costo base: L.${costoBase}`);
+console.log(`Costo por peso excedente: L.${pesoExcedente}`);
+console.log(`Costo total: L.${costoBase + pesoExcedente}`);
+console.log('\n++++++++++++++++++++++++++++++++++');
+
+
+
+
 
