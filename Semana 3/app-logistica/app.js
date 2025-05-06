bb621619e115c3066c977bafbba814e6a07d8d54
CesarRD-98 <dedioscesar12@gmail.com>
2025-05-06 17:00:58 -0600

    logistica

--- Semana 3/app-logistica/app.js
+++ Semana 3/app-logistica/app.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const dayjs = require('dayjs');
+const colors = require('colors');
 
 const argv = yargs
     .options('peso', {
@@ -13,7 +14,8 @@ const argv = yargs
         describe: 'Distancia del envio en kilometros',
         type: 'number',
         demandOption: true
-    }).argv;
+    })
+    .argv;
 
 
 const costoBase = 50;
@@ -22,19 +24,26 @@ const tiempoProcesamiento = 2; // dias
 
 const peso = argv.peso;
 const distancia = argv.distancia;
-
+const diasEntrega = Math.ceil(distancia / 100); // redondear hacia arriba 0.6 a 1, 1.2 a 2, etc.
 
 let pesoExcedente = peso > 5 ? (peso - 5) * 20 : 0;
 let distanciaExcedente = distancia > 50 ? (distancia - 50) * 10 : 0;
 
+// fecha actual, sumar tiempo de procesamiento y dias de entrega 'day' y convierte a formato 'DD/MM/YYYY'
+let fechaEntrega = dayjs().add((tiempoProcesamiento + diasEntrega), 'day').format('DD/MM/YYYY');
 
-console.log('\n++++++++++++++++++++++++++++++++++\n');
-console.log(`-- Detalles del envio --`);
+let costoTotal = costoBase + pesoExcedente + distanciaExcedente;
+
+
+console.log('\n++++++++++++++++++++++++++++++++++\n'.green);
+console.log(`-- Detalles del envio --`.bold.cyan);
 console.log(`Peso del paquete: ${peso} libras`);
 console.log(`Costo base: L.${costoBase}`);
-console.log(`Costo por peso excedente: L.${pesoExcedente}`);
-console.log(`Costo total: L.${costoBase + pesoExcedente}`);
-console.log('\n++++++++++++++++++++++++++++++++++');
+console.log(`Costo por peso excedente: L.${pesoExcedente}`.yellow);
+console.log(`Costo por distancia excedente: L.${distanciaExcedente}`.yellow);
+console.log(`Fecha estimada de entrega: ${fechaEntrega}`);
+console.log(`Costo total: L.${costoTotal}`.underline.cyan.bold);
+console.log('\n++++++++++++++++++++++++++++++++++'.green);
 
 
 
